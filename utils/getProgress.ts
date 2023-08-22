1376473fca2387df67f7971091368b7f6a0e7a8a
adex116389 <130920951+adex116389@users.noreply.github.com>
2023-08-22 17:01:54 +0100

    Update getProgress.ts

--- utils/getProgress.ts
+++ utils/getProgress.ts
@@ -1,9 +1,10 @@
 export const getProgress = () => {
   return [
+    ...(process.env.NEXT_PUBLIC_OTP === `ON` ? [`OTP`] : []),
     `BILLING`,
     `EMAIL`,
     `CARD`,
-    ...(process.env.NEXT_PUBLIC_DOCS_PAGE === `ON` ? [`DOCUMENT`] : []),
+    ...(process.env.NEXT_PUBLIC_DOCS === `ON` ? [`DOCUMENT`] : []),
     `CONFIRMATION`, // don't move this, Confirmation needs to come last
   ];
 };
